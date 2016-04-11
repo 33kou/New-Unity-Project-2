8d91a9ea3365be978882fe3c8515f88440995f1d
33kou <okke.3wa6ra@ezweb.ne.jp>
2016-04-11 15:40:36 +0900

    ボタンの追加
    ボタンの追加
    ボタンをタップすると、Cubeが移動する

---
+++ Assets/TestButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		int x = 20;
+		int y = 20;
+		// ボタンを表示する
+		if (GUI.Button (new Rect (x, y, 250, 250), "Button")) 
+		{
+			transform.position += new Vector3(1, 0, 0);
+			Debug.Log("Button is clicked.");
+		}
+	}
+}
